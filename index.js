bfc89d662e1aa3c70b42c2b843e0778ec6a7d769
Oguzhan <oguzhanbesiktepe@gmail.com>
2024-10-22 16:11:00 -0400

    Added 2 functions for Logic of Dealing Cards, and Entering the Casino. Added HTML webpages to distinguish both the Entrance and BlackJack webpages. Listed CSS webpages that are Linked and distinguished by classes.

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+let age = 21
+let message = ""
+
+//Checking if User is old enough to enter the Casino
+
+function Entrance(){
+    if(age < 21){
+        message = "You cannot enter the Casino, you don't meet the age requirement"
+    } else {
+        message = "Welcome into our Casino, hope you enjoy your stay!"
+    }
+}
+
 // Setting first and second card variables, w sum to see if user wins, loses, or needs to draw more cards
 
-let firstCard = 10
+let firstCard = 11
 let secondCard = 11
 let sum = firstCard + secondCard
-
-console.log(sum)
+let hasBlackJack = false
+let isAlive = true
 
 //Logic of drawing cards
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21){
-    console.log("You win! 🥳")
-} else if (sum > 21) {
-    console.log("Sorry you drew over 21, you lose! 😭")
+function startGame () {
+
+    if (sum <= 20){
+        message = "Do you want another card? 🙂"
+    } else if (sum === 21){
+        message = "Congratulations, here are your Chips 🥳"
+        hasBlackJack = true
+    } else {
+        message = "Sorry, you lose 😭"
+        isAlive = false
+    }
+    
+    console.log(message)
+
 }
