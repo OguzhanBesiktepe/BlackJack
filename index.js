23e6e4ad9edbdbb1faeae7104af31cec3c07dcff
Oguzhan <oguzhanbesiktepe@gmail.com>
2024-10-23 18:23:52 -0400

    Changes

--- index.js
+++ index.js
@@ -8,31 +8,38 @@ function Entrance(){
         message = "You cannot enter the Casino, you don't meet the age requirement"
     } else {
         message = "Welcome into our Casino, hope you enjoy your stay!"
+        window.location.href = "index.html"
     }
 }
 
 // Setting first and second card variables, w sum to see if user wins, loses, or needs to draw more cards
 
-let firstCard = 11
+let firstCard = 10
 let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
+let cardsEl = document.getElementById("cards-el")
+let messageEl = document.getElementById("message-el") //References HTML Document
+let sumEl = document.getElementById("sum-el")
 
 //Logic of drawing cards
 
 function startGame () {
 
+    cardsEl.textContent = "Cards: " + firstCard + "|" + secondCard
+    sumEl.textContent = "Sum: " + sum
+
     if (sum <= 20){
-        message = "Do you want another card? 🙂"
+        message = "Do you want another card?"
     } else if (sum === 21){
-        message = "Congratulations, here are your Chips 🥳"
+        message = "Congratulations, here are your Chips!"
         hasBlackJack = true
     } else {
-        message = "Sorry, you lose 😭"
+        message = "Sorry, you lose"
         isAlive = false
     }
-    
-    console.log(message)
+
+    messageEl.textContent = message //Stores content in message
 
 }
